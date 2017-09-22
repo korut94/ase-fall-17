ad0894e2af53c4d225039371c2fb410117504951
korut94 <andreamnt94@gmail.com>
2017-09-22 12:19:53 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ase-fall-17
+Repo for the all the Advance Software Engineering's projects during the laboratory
+
---
+++ src/CalculatorIF.java
@@ -0,0 +1,14 @@
+public interface CalculatorIF  {
+    /**
+     * Given two integers m and n, the function returns the result of m/n
+     */
+    int divide(int m, int n);
+    /**
+     * Given two integers m and n, the function returns the result of m*n
+     */
+    int multiplication(int m, int n);
+    /**
+     * Given two integers m and n, the function returns the result of m+n.
+     */
+    int sum(int m, int n);
+}
