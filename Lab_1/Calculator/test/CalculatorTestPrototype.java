bf11df656222cf4d16f3899e463948910de0331e
korut94 <andreamnt94@gmail.com>
2017-09-26 18:19:59 +0200

    Add several test for subtraction and multiplication

--- Lab_1/Calculator/test/CalculatorTestPrototype.java
+++ Lab_1/Calculator/test/CalculatorTestPrototype.java
@@ -4,11 +4,10 @@ import calculator.CalculatorPrototype;
 import org.junit.Test;
 
 public class CalculatorTestPrototype {
-    @Test
-    public void testSum() {
+    @Test(expected = ArithmeticException.class)
+    public void testDivisionByZero() {
         CalculatorPrototype calculator = new CalculatorPrototype();
-        int sum = calculator.sum(3,3);
-        assertEquals(6, sum);
+        int division = calculator.divide(3,0);
     }
 
     @Test
@@ -25,9 +24,56 @@ public class CalculatorTestPrototype {
         assertEquals(2, division);
     }
 
-    @Test(expected = ArithmeticException.class)
-    public void testDivisionByZero() {
+    @Test
+    public void testMultiplication() {
         CalculatorPrototype calculator = new CalculatorPrototype();
-        int division = calculator.divide(3,0);
+        int multiplication = calculator.multiply(4, 3);
+        assertEquals(12, multiplication);
+    }
+
+    @Test
+    public void testMultiplicationByOne() {
+        CalculatorPrototype calculator = new CalculatorPrototype();
+        int multiplication = calculator.multiply(7, 1);
+        assertEquals(7, multiplication);
+    }
+
+    @Test
+    public void testMultiplicationByZero() {
+        CalculatorPrototype calculator = new CalculatorPrototype();
+        int multiplication = calculator.multiply(7, 0);
+        assertEquals(0, multiplication);
+    }
+
+    @Test
+    public void testMultiplicationCommutative() {
+        CalculatorPrototype calculator = new CalculatorPrototype();
+        int x = 10, y = 6;
+        int mult1 = calculator.multiply(x, y);
+        int mult2 = calculator.multiply(y, x);
+        assertEquals(mult1, mult2);
+    }
+
+    @Test
+    public void testMultiplicationCommutativeByZero() {
+        CalculatorPrototype calculator = new CalculatorPrototype();
+        int mult1 = calculator.multiply(1, 0);
+        int mult2 = calculator.multiply(0, 9);
+        assertEquals(0, mult1);
+        assertEquals(0, mult2);
+    }
+
+    @Test
+    public void testSubtraction() {
+        CalculatorPrototype calculator = new CalculatorPrototype();
+        int sub = calculator.subtract(5, 1);
+        assertEquals(4, sub);
+    }
+
+    @Test
+    public void testSum() {
+        CalculatorPrototype calculator = new CalculatorPrototype();
+        int sum = calculator.sum(3,3);
+        assertEquals(6, sum);
     }
 }
