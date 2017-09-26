68bb6c84568421b00b4417a19fc896d107b2c956
korut94 <andreamnt94@gmail.com>
2017-09-26 18:22:17 +0200

    Space

--- Lab_1/Calculator/src/calculator/CalculatorIF.java
+++ Lab_1/Calculator/src/calculator/CalculatorIF.java
@@ -9,7 +9,6 @@ public interface CalculatorIF {
      * @return Given two integers, m and n, returns the result of m*n
      */
     int multiply(int m, int n);
-
     /**
      * @return Given two integers, m and n, returns the result of m-n
      */
